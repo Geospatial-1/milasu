f9884fa3836908759c4bde47455fd597df782957
@Geospatial-1 <136714288+Geospatial-1@users.noreply.github.com>
2025-06-20 02:25:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,16 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/app.js',
-  '/css/styles.css',
-  '/images/logo.png'
+  '/styles.css',
+  '/images/logo.png',
+  '/images/profile-placeholder.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
